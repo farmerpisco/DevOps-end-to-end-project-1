9a0b3ac1f1eee1e361e7b27d06834b519de09918
Paul Adegoke <farmerpisco@gmail.com>
2026-01-14 20:51:21 +0100

    Create ecr module

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,34 @@
+resource "aws_ecr_repository" "dev_repo" {
+  for_each = toset(var.repositories)
+  
+  name                 = "${var.project_name}-${each.value}"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "dev_repos" {
+  for_each   = aws_ecr_repository.dev_repo
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 10 images"
+      selection = {
+        tagStatus     = "any"
+        countType     = "imageCountMoreThan"
+        countNumber   = 10
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
